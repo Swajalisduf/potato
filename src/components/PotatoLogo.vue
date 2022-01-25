677c4a26e74e02bfc877b3aafc610b5cf0cd826e
Mike Kim <stonesgivelight@gmail.com>
2022-01-24 19:35:45 -0700

    initial commit

---
+++ src/components/PotatoLogo.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="icon-container">
+    <div class="potato-border">
+    <img alt="Potato logo" src="../assets/potato.png" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.icon-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+.potato-border {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 4px solid;
+  border-radius: 50%;
+  width: 100%;
+  height: 100%;
+  img {
+    width: 70%;
+    height: 70%;
+  }
+}
+</style>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import PotatoLogo from '~/components/PotatoLogo.vue'
+</script>
+
+<template>
+  <div class="logo-container">
+    <PotatoLogo />
+  </div>
+  <div class="site-title">
+    <h1>Get Baked!</h1>
+    <h2>We probably sell potatoes</h2>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.logo-container {
+  width: 150px;
+  height: 150px;
+}
+</style>
